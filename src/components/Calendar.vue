2616806941e2d99cf63e175018ab8c9b4b2291f6
Sam Rechsteiner <srechsteiner@ignitexds.com>
2020-11-14 13:09:35 -0500

    add an event to the calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
-    <v-calendar type="week"></v-calendar>
+    <v-calendar type="week" v-bind:events="events"></v-calendar>
   </div>
 </template>
 
 <script>
+let start = new Date("11-13-2020 1:00")
+let end = new Date(start)
+end.setHours(start.getHours() + 3)
+
+export default {
+  data: function() {
+    return {
+      events: [
+        {
+          name: 'this is an event',
+          start: start,
+          end: end,
+          color: 'green',
+          timed: true
+        }
+      ]
+    }
+  }
+}
 </script>
