afbcf292ddb880aeb53640ea56b6980ccf8e4202
Sam Rechsteiner <srechsteiner@ignitexds.com>
2020-11-15 18:43:01 -0500

    add navigation by text field and date picker

--- src/components/CalendarNavigator.vue
+++ src/components/CalendarNavigator.vue
@@ -7,6 +7,26 @@
       >
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
+      <v-menu
+        v-model="isMenuOpen"
+        transition="scale-transition"
+        offset-y
+        :close-on-content-click="false"
+      >
+        <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-bind:value="value"
+                readonly
+                prepend-icon="mdi-calendar"
+                v-bind="attrs"
+                v-on="on"
+            ></v-text-field>
+        </template>
+        <v-date-picker
+            v-bind:value="value"
+            @input="$emit('update:value', $event); isMenuOpen = false"
+        />
+      </v-menu>
       <v-btn
         icon
         class="ma-2"
@@ -16,3 +36,16 @@
       </v-btn>
     </v-row>
 </template>
+
+<script>
+export default {
+    props: {
+        value: String
+    },
+    data: function() {
+        return {
+            isMenuOpen: false
+        }
+    }
+}
+</script>
