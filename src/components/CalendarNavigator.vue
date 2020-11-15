7fac37c2aff597620667711a18e3a87889612135
Sam Rechsteiner <srechsteiner@ignitexds.com>
2020-11-15 17:09:13 -0500

    move calendar navigation to separate component

---
+++ src/components/CalendarNavigator.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-row justify="center">
+      <v-btn
+        icon
+        class="ma-2"
+        @click="$emit('timeTravelPrev')"
+      >
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        class="ma-2"
+        @click="$emit('timeTravelNext')"
+      >
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-row>
+</template>
