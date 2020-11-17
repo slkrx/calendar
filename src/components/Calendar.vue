9a365747231ea3cedad093e06ac283a2d4be81cc
Sam Rechsteiner <srechsteiner@ignitexds.com>
2020-11-16 23:04:53 -0500

    add task view

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -14,7 +14,19 @@
             v-bind:events="events"
             v-model="value"
             v-bind:event-color="getEventColor"
+            v-on:click:event="showEvent"
           ></v-calendar>
+          <v-menu
+              v-model="selectedOpen"
+              :close-on-content-click="false"
+              :activator="selectedElement"
+              offset-x
+          >
+            <CalendarEventShow
+              v-bind.sync="selectedEvent"
+              v-bind:selectedOpen.sync="selectedOpen"
+            />
+          </v-menu>
         </v-sheet>
       </v-col>
     </v-row>
@@ -23,30 +35,65 @@
 
 <script>
 import CalendarNavigator from './CalendarNavigator'
+import CalendarEventShow from './CalendarEventShow'
 
 let start = new Date()
+start.setHours(1)
 let end = new Date(start)
 end.setHours(start.getHours() + 3)
 
 export default {
-  components: { CalendarNavigator },
+  components: {
+    CalendarNavigator,
+    CalendarEventShow
+  },
   data: function() {
     return {
       value: new Date().toISOString().substr(0, 10),
       events: [
         {
-          name: 'this is an event',
+          name: 'event',
           start: start,
           end: end,
           color: 'orange',
-          timed: true
+          timed: true,
+          description: 'this is an event'
+        },
+        {
+          name: 'Walk the dog',
+          start: new Date("November 17, 2020 2:00:00"),
+          end: new Date("November 17, 2020 3:00:00"),
+          color: 'blue',
+          timed: true,
+          description: 'After you feed her you need to take her out.'
         }
-      ]
+      ],
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false
     }
   },
   methods: {
     getEventColor(event) {
       return event.color
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event
+        this.selectedElement = nativeEvent.target
+        setTimeout(() => {
+          this.selectedOpen = true
+        }, 10)
+      }
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false
+        setTimeout(open, 10)
+      } else {
+        open()
+      }
+
+      nativeEvent.stopPropagation()
     }
   }
 }
---
+++ src/components/CalendarEventShow.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card
+        color="grey lighten-4"
+        min-width="350px"
+        flat
+    >
+        <v-toolbar
+            :color="color"
+            dark
+        >
+            <v-toolbar-title v-html="name"></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn
+                icon
+                @click="$emit('update:selectedOpen', false)"
+            >
+                <v-icon>mdi-close-circle</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-card-text>
+            <v-textarea
+                :value="description"
+                @input="$emit('update:description', $event)"
+            ></v-textarea>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        selectedOpen: Boolean,
+        color: String,
+        name: String,
+        description: String
+    }
+}
+</script>
