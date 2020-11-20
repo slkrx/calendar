288ef3fa122087d325276e69ca91c5b07422763b
Sam Rechsteiner <srechsteiner@ignitexds.com>
2020-11-19 22:23:39 -0500

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <v-app>
-    <v-main>
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-tabs
+          centered
+          class="ml-n9"
+          color="grey darken-1"
+      >
+        <v-tab>
+          Calendar
+        </v-tab>
+        <v-tab>
+          Statistics
+        </v-tab>
+      </v-tabs>
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+      ></v-avatar>
+    </v-app-bar>
+    <v-main class="grey lighten-3">
       <Calendar/>
     </v-main>
   </v-app>
