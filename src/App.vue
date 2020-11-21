f7952b607b6920e9562c7f024af353813deeef2c
Sam Rechsteiner <samrech@umich.edu>
2020-11-20 19:50:47 -0500

    move appBar to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-tabs
-          centered
-          class="ml-n9"
-          color="grey darken-1"
-      >
-        <v-tab>
-          Calendar
-        </v-tab>
-        <v-tab>
-          Statistics
-        </v-tab>
-      </v-tabs>
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
+    <AppBar/>
     <v-main class="grey lighten-3">
       <Calendar/>
     </v-main>
@@ -31,12 +9,14 @@
 
 <script>
 import Calendar from './components/Calendar';
+import AppBar from './components/AppBar'
 
 export default {
   name: 'App',
 
   components: {
     Calendar,
+    AppBar
   },
 
   data: () => ({
