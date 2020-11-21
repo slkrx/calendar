b7d906b2beb821d757cc7b1dfd34fd97ec2acabf
Sam Rechsteiner <samrech@umich.edu>
2020-11-20 22:03:08 -0500

    install vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ Vue.use(require('vue-moment'))
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
