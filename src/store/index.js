ac278737d08b088480aab35bae07f84e2339d685
tandunn <tandunn@umich.edu>
2020-11-29 14:54:31 -0500

    Added local storage for events in the browser

--- src/store/index.js
+++ src/store/index.js
@@ -11,13 +11,38 @@ export default new Vuex.Store({
   },
   mutations: {
     createEvent(state, event) {
-      state.events.push(event)
+      state.events.push(event);
+      localStorage.setItem('events', JSON.stringify(state.events));
     },
     updateSelectedDate(state, newDate) {
-      state.selectedDate = newDate
+      state.selectedDate = newDate;
     },
     updateCalendarType(state, newType) {
-      state.calendarType = newType
+      state.calendarType = newType;
+    },
+    saveData() {
+
+    },
+    initialiseStore(state) {
+      // Fetches data stored in the browser when the app is started
+      if (localStorage.getItem('events')) {
+        try {
+          // A really ugly regular expression for modifying the json parser to allow for Date types
+          let reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+          JSON.dateParser = function (key, value) {
+            if (typeof value === 'string') {
+              let arr = reISO.exec(value);
+              if (arr)
+                return new Date(value);
+            }
+            return value;
+          };
+          state.events = JSON.parse(localStorage.getItem('events'), JSON.dateParser);
+        } catch(e) {
+          localStorage.removeItem('events');
+          console.error("Events data failed parsing");
+        }
+      }
     }
   },
   actions: {
