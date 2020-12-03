f0b78282798cab2b63e319ba313122e3a12d75d9
Sam Rechsteiner <samrech@umich.edu>
2020-12-03 00:51:23 -0500

    style todays tasks

--- src/views/TodaysTasks.vue
+++ src/views/TodaysTasks.vue
@@ -1,48 +1,65 @@
 <template>
     <v-container>
-        <h2 class="display-1 success--text pl-4">
-            Today's Tasks: {{ todaysTasks.length }}
-        </h2>
-        <v-divider class="mt-4"></v-divider>
-        <v-row
-            class="my-1"
-            align="center"
-        >
-            <strong class="mx-4 info--text text-darken-2">
-                Remaining: {{ remainingTasks.length }}
-            </strong>
-
-            <v-divider vertical></v-divider>
-
-            <strong class="mx-4 success--text text--darken-2">
-                Completed: {{ completedTasks.length }}
-            </strong>
-
-            <v-spacer></v-spacer>
-
-            <v-progress-circular
-                :value="progress"
-                class="mr-2"
-            ></v-progress-circular>
+        <v-row>
+            <v-col cols="4">
+                <v-sheet rounded elevation="10">
+                    <v-row>
+                        <v-col cols="12">
+                            <h2 class="display-1 pl-4">
+                                {{ todaysTasks.length }} Tasks for Today
+                            </h2>
+                        </v-col>
+                        <v-col cols="12">
+                            <h2 class="mx-4 mb-2 success--text text--darken-2">
+                                Completed:
+                                <v-fade-transition leave-absolute>
+                                    <span :key="`completed-${completedTasks.length}`">
+                                        {{ completedTasks.length }}
+                                    </span>
+                                </v-fade-transition>
+                            </h2>
+                            <h2 class="mx-4 mb-2 info--text text-darken-2">
+                                Remaining:
+                                <v-fade-transition leave-absolute>
+                                    <span :key="`remaining-${remainingTasks.length}`">
+                                        {{ remainingTasks.length }}
+                                    </span>
+                                </v-fade-transition>
+                            </h2>
+                        </v-col>
+                    </v-row>
+                            <v-progress-linear
+                                :value="progress"
+                                height="25"
+                                :color="progressColor"
+                            >
+                                <strong>{{ Math.ceil(progress) }}%</strong>
+                            </v-progress-linear>
+                </v-sheet>
+            </v-col>
+            <v-col>
+                <v-sheet rounded class="px-10" elevation="10">
+                    <v-timeline>
+                        <v-timeline-item
+                            v-for="task in todaysTasks"
+                            :key="task.id"
+                            :color="task.color"
+                            fill-dot
+                        >
+                            <template v-slot:opposite>
+                                <span
+                                    class="headline font-weight-bold"
+                                    :style="{ color: task.color }"
+                                >
+                                    {{ task.start | moment("h:mm a") }}
+                                </span>
+                            </template>
+                            <Task :taskId="task.id"/>
+                        </v-timeline-item>
+                    </v-timeline>
+                </v-sheet>
+            </v-col>
         </v-row>
-        <v-timeline>
-            <v-timeline-item
-                v-for="task in todaysTasks"
-                :key="task.id"
-                :color="task.color"
-                fill-dot
-            >
-                <template v-slot:opposite>
-                    <span
-                        class="headline font-weight-bold"
-                        :style="{ color: task.color }"
-                    >
-                        {{ task.start | moment("h:mm a") }}
-                    </span>
-                </template>
-                <Task :taskId="task.id"/>
-            </v-timeline-item>
-        </v-timeline>
     </v-container>
 </template>
 
@@ -64,6 +81,14 @@ export default {
         },
         progress() {
             return this.completedTasks.length / this.todaysTasks.length * 100
+        },
+        progressColor() {
+            if (this.progress <= 25)
+                return 'info'
+            else if (this.progress <= 75)
+                return 'amber'
+            else
+                return 'green'
         }
     }
 }
